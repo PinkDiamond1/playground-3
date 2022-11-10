c9ea1ac4afab2977b2ffbb2c508334ec14aa9fd7
ZGrguric <zgrguric@xrplwin.com>
2022-11-10 15:13:18 +0100

    Update index.blade.php

--- resources/views/txmutationparser/index.blade.php
+++ resources/views/txmutationparser/index.blade.php
@@ -230,7 +230,9 @@ function runAnimationQueue()
   },((step+1)*2500));
 }
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
 }
 function reverseObjectIntoArray(obj){
   var array = [];
